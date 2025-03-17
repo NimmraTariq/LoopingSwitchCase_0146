18a0fdf7193ac28497282df59279c59582102ac6
NimmraTariq <nimra369tariq@gmail.com>
2025-03-17 20:49:22 +0700

    add switch function

--- LoopingSwitchCase_0146/switchcase.cpp
+++ LoopingSwitchCase_0146/switchcase.cpp
@@ -12,3 +12,14 @@ int main(){
     int pilihan;
     float panjang, lebar;
     float jarijari;
+
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
